a9adcf0b297f376b91b2ebf49e49d4505269f3ef
ashish221306 <ashishbhu221306@gmail.com>
2023-03-08 15:27:02 +0530

    fixed

--- components/post.jsx
+++ components/post.jsx
@@ -1,40 +1,46 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "@/styles/post.module.css";
+import { useRouter } from "next/router";
 
 const Post = ({ remove, edit, post }) => {
+  const router = useRouter();
   return (
-    <Link
-      key={post._id}
-      href={`/blog/${post.slug}`}
-      as={`/blog/${post.slug}`}
-      passHref
-    >
-      <a
-        title={post.title}
-        style={{ textDecoration: "none" }}
-        className={styles.post}
-        key={post._id}
-      >
-        <span className={styles.thumbnail}>
-          <Image
-            objectFit="cover"
-            layout="fill"
-            priority
-            alt=""
-            src={
-              post.featuredImage
-                ? post.featuredImage?.node.mediaDetails.sizes[3]?.sourceUrl
-                : "/images/default.png"
-            }
-          ></Image>
-        </span>
-        <div className={styles.content}>
-          <h3> {post.title}</h3>
-          <div dangerouslySetInnerHTML={{ __html: post.excerpt }} />
-        </div>
-      </a>
-    </Link>
+    <>
+      {!router.asPath.includes(post.slug) && (
+        <Link
+          key={post._id}
+          href={`/blog/${post.slug}`}
+          as={`/blog/${post.slug}`}
+          passHref
+        >
+          <a
+            title={post.title}
+            style={{ textDecoration: "none" }}
+            className={styles.post}
+            key={post._id}
+          >
+            <span className={styles.thumbnail}>
+              <Image
+                objectFit="cover"
+                layout="fill"
+                priority
+                alt=""
+                src={
+                  post.featuredImage
+                    ? post.featuredImage?.node.mediaDetails.sizes[3]?.sourceUrl
+                    : "/images/default.png"
+                }
+              ></Image>
+            </span>
+            <div className={styles.content}>
+              <h3> {post.title}</h3>
+              <div dangerouslySetInnerHTML={{ __html: post.excerpt }} />
+            </div>
+          </a>
+        </Link>
+      )}
+    </>
   );
 };
 
