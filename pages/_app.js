4759fffbccb598856f3c6cac183da3f13299e4e0
ashish221306 <ashishbhu221306@gmail.com>
2023-07-27 19:54:08 +0530

    k

--- pages/_app.js
+++ pages/_app.js
@@ -23,7 +23,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     // <SessionProvider session={session} basePath="/api/auth">
       <Provider store={store}>
-        <div className="loading-lcp"></div>
+        <div className="loading-lcp">loading</div>
         {!layout.landing.includes(router.pathname) ? (
           <Layout>
             <Component {...pageProps} />
