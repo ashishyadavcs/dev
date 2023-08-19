5cd0d0157821c4b08a2a79438b68000c7c3f0bfa
ashish221306 <ashishbhu221306@gmail.com>
2023-08-19 06:23:12 +0530

    home

---
+++ styles/home.js
@@ -0,0 +1,75 @@
+import { media } from "config/device";
+import styled from "styled-components";
+const Styled = {
+    Banner: styled.div`
+        background: linear-gradient(rgb(247 239 239), rgb(217 240 237));
+        height: 92vh;
+        max-height: 700px;
+        position: relative;
+        display: flex;
+        align-items: center;
+        ${media.md} {
+            &:before {
+                content: "";
+                height: 100%;
+                width: 60%;
+                transition: all 1s;
+                position: absolute;
+                right: 0;
+                top: 0;
+                clip-path: polygon(27% 0, 100% 0, 100% 100%, 0% 100%);
+                background: linear-gradient(to left, red, #ffecb300);
+            }
+        }
+        ${media.sm}{
+            padding: 2rem 0;
+            display: block;
+            height: unset;
+            max-height: unset;
+        }
+        ${media.xs} {
+            padding: 2rem 0;
+            display: block;
+            height: unset;
+            max-height: unset;
+        }
+    `,
+    Landing: styled.div`
+        padding: 2rem 0;
+        background: rgb(245 252 255);
+        ${media.xs} {
+            [class*="row"]:nth-child(3) {
+                flex-direction: column-reverse;
+            }
+        }
+        h2 {
+            font-size: clamp(40px, 3vw, 50px) !important;
+        }
+    `,
+    Cta: styled.a`
+        display: inline-block;
+        padding: 15px 30px;
+        background: red;
+        color: aliceblue;
+        border-radius: 8px;
+        text-align: center;
+        margin: 40px 0;
+        font-weight: 600;
+        width: 260px;
+        text-decoration: none;
+        transition: all 0.3s;
+
+        &:hover {
+            text-decoration: none;
+            color: #fff;
+            ${media.md} {
+            transform: translateY(-6px) scale(1.1);
+        }
+        }
+       
+        ${media.xs} {
+            width: 100%;
+        }
+    `,
+};
+export default Styled;
