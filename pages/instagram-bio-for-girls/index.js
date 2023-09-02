67e9548874b0b218bf6b8d918f30d7937f768089
ashish221306 <ashishbhu221306@gmail.com>
2023-09-02 17:08:29 +0530

    ig bio girls

---
+++ pages/instagram-bio-for-girls/index.js
@@ -0,0 +1,95 @@
+import {NextSeo} from "next-seo";
+import { useEffect } from "react";
+import { styled } from "styled-components";
+import { clickToCopy } from "utils/common";
+
+const Page = () => {
+    useEffect(() => {
+        document.querySelectorAll(".biolist li").forEach(li => (li.onclick = clickToCopy));
+    }, []);
+    return (
+        <div className="container  my-4">
+            <NextSeo title="# Best Instagram Bio For Girls" description="Generate best instagram bio for girls , top 100 best instagram bio for first impression"/>
+            <h1 className="text-center">Craft the perfect<br></br> Instagram Bio For Girls</h1>
+            <span className="notice">click to copy</span>
+            <Biolist className="biolist">
+                <li>"Sassy but classy 💁‍♀️"</li>
+                <li>"Living life one filter at a time 📸"</li>
+                <li>"Smiles are always in fashion 😄💄"</li>
+                <li>"Chasing dreams and sunsets 🌅✨"</li>
+                <li>"Confidence level: Selfie with no filter 🤳"</li>
+                <li>"Making the days count, not counting the days ⏳"</li>
+                <li>"Dancing through life's ups and downs 💃"</li>
+                <li>"Happiness is homemade 🏡❤️"</li>
+                <li>"On a journey to discover my own magic ✨🌟"</li>
+                <li>"Life is short, make every hair flip count 💁‍♀️💅"</li>
+                <li>"Queen of my own world 👑🌍"</li>
+                <li>"Creating my own sunshine on cloudy days ☀️"</li>
+                <li>"In a relationship with adventures and good vibes only 🌍✌️"</li>
+                <li>"I believe in pink and a positive attitude 💖🌸"</li>
+                <li>"Just a girl with a heart full of wanderlust ✈️🌎"</li>
+                <li>"Living the moments that make the best memories 📷🎉"</li>
+                <li>"Strong women lift each other up 👭💪"</li>
+                <li>"Chasing dreams, not boys 🌟💫"</li>
+                <li>"Elegance is when the inside is as beautiful as the outside 💫👗"</li>
+                <li>"Turning my dreams into plans 🌠📝"</li>
+            </Biolist>
+
+            <p>
+                Top instagram bios for girls to decorate and make your instagram profile
+                outstanding. Select and copy styles for your instagram bio
+            </p>
+            <strong>Top 100 instagram bio for girls</strong>
+            <h2>How to use instagram bio for girls tool</h2>
+            <p>
+                It is very easy to use this tool . There are two ways to use this tool either you
+                can use use readymade style or you can make you own style from this tool for each
+                line for your bio.
+            </p>
+            <h2>Why to use this instagram bio tool</h2>
+            <p>
+                In this era of social media instagram is the most famous social site for young
+                peoples over the globe.To make the first impression on your instagram profile
+                visitor it is most important to make your instagram bio look most attractive and
+                professional.
+            </p>
+        </div>
+    );
+};
+
+export default Page;
+const Biolist = styled.ul`
+    list-style: none;
+    li {
+        padding: 10px;
+        border-radius: 8px;
+        width: max-content;
+        min-width: 300px;
+        text-align: center;
+        border: 3px solid teal;
+        margin: 10px auto;
+        transition: all 0.3s;
+        cursor: pointer;
+        position: relative;
+        &:hover{
+            background: teal;
+            color: #fff;
+        }
+        &:active{
+            &::before{
+                content: "copied";
+                position: absolute;
+                top: 50%;
+                transition: all 2s;
+                transition-delay: .3s;
+                left: 150%;
+                padding: 10px;
+                border-radius: 8px;
+                display: inline-block;
+                background: #000;
+                color: #fff;
+            }
+        }
+       
+    }
+`;
