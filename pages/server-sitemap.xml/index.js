fffb8ee039204c989592943a7c3fb0202f251eba
ashish221306 <ashishbhu221306@gmail.com>
2023-09-02 21:49:15 +0530

    fixed

--- pages/server-sitemap.xml/index.js
+++ pages/server-sitemap.xml/index.js
@@ -4,15 +4,15 @@ import { getServerSideSitemap } from "next-sitemap";
 export const getServerSideProps = async (ctx) => {
   const posts = await getPostSlugs();
   const categories = await getCategorySlugs();
-  // const path1 = posts.map((post) => ({
-  //   loc: `${process.env.APP_URL}/blog/${post.slug}`,
-  //   lastmod: new Date().toISOString(),
-  // }));
+  const path1 = posts.map((post) => ({
+    loc: `${process.env.APP_URL}/blog/${post.slug}`,
+    lastmod: new Date().toISOString(),
+  }));
   // const path2 = categories.map((post) => ({
   //   loc: `${process.env.APP_URL}/blog/category/${post.params.categoryName}`,
   //   lastmod: new Date().toISOString(),
   // }));
-  const fields = [];
+  const fields = [...path1];
   return getServerSideSitemap(ctx, fields);
 };
 
