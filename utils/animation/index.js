8f05da049c56f94af4b74bc10d1ccf3acf7b6b90
ashish221306 <ashishbhu221306@gmail.com>
2023-10-14 16:32:20 +0530

    fixed

---
+++ utils/animation/index.js
@@ -0,0 +1,19 @@
+export const animateList=(selector)=>{
+    if(typeof document=='undefined') return
+const slides = document.querySelectorAll(selector);
+slides[0].classList.add("active")
+slides.forEach((li, index) => {
+    li.onclick = e => {
+        slides.forEach(li => li.classList.remove("active"));
+        li.classList.add("active");
+    };
+    li.addEventListener("animationend", () => {
+        li.classList.remove("active");
+        if (li.nextElementSibling) {
+            li.nextElementSibling.classList.add("active");
+        } else {
+            slides[0].classList.add("active");
+        }
+    });
+});
+}
