b1c0f32305a8f9e1ad8a2d44279fd0cfad0b9a1a
ashish221306 <ashishbhu221306@gmail.com>
2023-11-06 17:15:59 +0530

    fixed

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home({posts}) {
                         <div className="col-md-7 col-12">
                             
                             <h1 className="gradient-text heading text-capitalize">
-                                Looking for Help<br></br> For Your website<br></br> development
+                                Looking for a developer
                                 <br></br>
                               
                             </h1>
