d1cded40d5d41335fdb806cb6fe6b90de8ed45cd
Ashish Kumar <ashishbhu221306@gmail.com>
2023-12-02 17:30:36 +0530

    fixed

--- pages/vrc.js
+++ pages/vrc.js
@@ -1,13 +1,15 @@
 import { media } from "config/device";
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
+import { FaPlay, FaStop } from "react-icons/fa";
 import styled from "styled-components";
 
 const Page = () => {
     var recorder;
-    const [chunks, setchunks] = useState([])
-    try {
+    var chunks = [];
+    const [files, setFiles] = useState([]);
+    const [state, setState] = useState(false);
+      if(typeof document=='undefined') return
         const video = document.querySelector("video");
-        if (typeof navigator == "undefined") return;
         navigator.mediaDevices
             .getUserMedia({
                 audio: true,
@@ -15,85 +17,114 @@ const Page = () => {
             })
             .then(stream => {
                 video.srcObject = stream;
-                recorder=new MediaRecorder(stream)
-                recorder.addEventListener('dataavailable',e=>{
-                    setchunks(prev=>[...prev,e.data])
-                })
+                recorder = new MediaRecorder(stream, { mimeType: "video/webm; codecs=vp9" });
+                recorder.start()
+                recorder.onstart = e => {
+                    chunks = [];
+                };
+                recorder.ondataavailable = e => {
+                    console.log(e);
+                    chunks.push(e.data);
+                };
+                recorder.onstop = e => {
+                    console.log("stop", e);
+                    const blob = new Blob(chunks, { type: "video/mp4" });
+                    setFiles(prev => [...prev, blob]);
+                };
             });
-    } catch {}
-    const startRecord=()=>{
-        recorder.start()
-    }
-    const stopRecord=()=>{
-        recorder.stop()
-    }
+
+
+    const record = () => {
+        if (typeof recorder == "undefined") return;
+       recorder.stop()
+        setState(!state)
+    };
     return (
-        <Pagestyle className="vrc container">
-            <ul className="filter">
-                {[...Array(3)].map(el => {
-                    return <li></li>;
-                })}
-            </ul>
-            <video autoPlay src="https://www.youtube.com/watch?v=F2t5Ji8hsD4"></video>
-            <ul className="action">
-                {[...Array(2)].map(el => {
-                    return (
+        <Pagestyle className="vrc">
+            <div className="saved">
+                <div className="header">
+                    <span>Recorded files</span>
+                </div>
+                <ul className="files">
+                    {files.map(el => (
                         <li>
-                            <button title="capture"></button>
+                            <video controls autoPlay src={URL.createObjectURL(el)}></video>
                         </li>
-                    );
-                })}
-            </ul>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="camera">
+                <div className="header">
+                    <h2>recorder</h2>
+                </div>
+                <div className="recorder">
+                    <video autoPlay src="https://www.youtube.com/watch?v=F2t5Ji8hsD4"></video>
+                </div>
+            </div>
+            <div className="action">
+                <button
+                    className="theme-btn"
+                    onClick={e => {
+                        e.currentTarget.classList.toggle("active");
+                        record();
+                    }}
+                    title="capture"
+                >
+                    {state ? "start" : "Stop"}
+                </button>
+            </div>
         </Pagestyle>
     );
 };
 
 export default Page;
 const Pagestyle = styled.div`
-    display: flex;
+    background: #4fffb0;
+    box-sizing: border-box;
+    height: 90vh;
+    .header {
+        position: sticky;
+        top: 0;
+        background: #fff;
+        padding: 10px;
+        z-index: 2;
+        font-size: 18px;
+    }
+    * {
+        transition: all 0.3s;
+    }
+    --gap: 20px;
+    display: grid;
+    grid-template-columns: 20% calc(60% - var(--gap) * 2) 20%;
     align-items: center;
+    gap: var(--gap);
 
-    ul {
-        display: flex;
-        flex-direction: column;
-
-        &.filter {
-            height: 100%;
-            li {
-                border-radius: 10px;
-                height: 150px;
-                width: 200px;
-                background: #ddd;
-                margin-bottom: 10px;
-            }
+    .camera {
+        .header {
+            text-align: center;
+            color: red;
         }
-        li {
-            display: inline-block;
-            button {
-                height: 50px;
-                width: 50px;
-                background: red;
-                border-radius: 50%;
-                border: 10px solid #ddd;
-            }
-        }
-        &.action {
-            gap: 20px;
-            li:last-child {
-                background: #fff;
-            }
+        .recorder {
+            border-radius: 10px;
+            overflow: hidden;
+            min-height: 500px;
+            max-width: 100%;
         }
     }
-    video {
-        height: 500px;
-        width: 100%;
-    }
-    ${media.sm} {
-        flex-direction: column;
-        ul{
-            flex-direction: row;
-            flex-wrap: wrap;
-            li{width:100%}
+    .saved {
+        overflow: auto;
+        height: 100%;
+        ul {
+            padding: 10px;
         }
+        video {
+            width: 100%;
+        }
+    }
+    .action {
+    }
+    ${media.sm}{
+        grid-template-columns: auto;
     }
 `;
