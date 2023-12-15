9a3963fcee3c2d06599f91ca7d8b79bacee6e79c
Ashish Kumar <ashishbhu221306@gmail.com>
2023-12-15 16:13:05 +0530

    page added

---
+++ pages/demo/chat.js
@@ -0,0 +1,11 @@
+import Chat from "@/components/chat";
+
+const Page = () => {
+    return (
+        <div className="container" style={{ minHeight: "80vh" }}>
+            <Chat />
+        </div>
+    );
+};
+
+export default Page;
