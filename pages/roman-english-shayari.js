3d452383b51b1550aedcb3c813155b06afc9b1ec
Ashish Kumar <ashishbhu221306@gmail.com>
2024-01-27 23:56:19 +0530

    fixed

---
+++ pages/roman-english-shayari.js
@@ -0,0 +1,207 @@
+import Toc from "@/components/tableofcontent";
+import { NextSeo } from "next-seo";
+import styles from "@/styles/shayari.module.css";
+import * as htmlToImage from "html-to-image";
+import Image from "next/image";
+import { useState } from "react";
+import { MdCloudDownload, MdContentCopy } from "react-icons/md";
+import { romantic,shayaries,friend ,mohabbat} from "public/data/shayari";
+
+import DownloadPopup from "@/components/download-popup";
+const Page = ({ data=[] }) => {
+
+
+  const copy = (text) => {
+    const input = document.createElement("textarea");
+    input.value = text;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand("copy");
+    input.remove();
+    document
+      .querySelector(`.${styles.copied}`)
+      .classList.add(`${styles.active}`);
+    setTimeout(() => {
+      document
+        .querySelector(`.${styles.copied}`)
+        .classList.remove(`${styles.active}`);
+    }, 1200);
+  };
+  const [download, setdownload] = useState(false);
+  const toPng = (node) => {
+    htmlToImage
+      .toPng(node)
+      .then(function (dataUrl) {
+        setdownload({ name: "shayari", url: dataUrl });
+      })
+      .catch(function (error) {});
+  };
+  return (
+    <div className={`container article my-2 ${styles.shayaries}`}>
+      <span className={styles.copied}>copied</span>
+      <NextSeo
+        title="Top 10 The Best Shayari in roman english 🔥"
+        description="Top shayari in roman english with english & roman meaning, read collection of roman english shayari copy & download"
+      />
+      <article className="col">
+        <h1 className="heading my-2 text-center">Shayari in roman english</h1>
+        <p className="text-center">
+          Top shayari in roman english with english & roman meaning, read
+          collection of roman english shayari copy & download
+        </p>
+        <div className={styles.items}>
+          {data.map((card, i) => (
+            <div className={styles.shayari}>
+              <div className={styles.action}>
+                <MdCloudDownload
+                  onClick={(e) =>
+                    toPng(e.currentTarget.parentElement.parentElement)
+                  }
+                  size={25}
+                />
+                <MdContentCopy
+                  onClick={(e) =>
+                    copy(e.currentTarget.parentElement.parentElement.innerText)
+                  }
+                  size={25}
+                />
+              </div>
+              <Image
+                className={styles.img}
+                priority
+                src={`/shayari/romantic-shayari-3.png`}
+                layout="fill"
+                objectFit="cover"
+                alt="shayari in roman english"
+              />
+              <div dangerouslySetInnerHTML={{ __html: card.node.content }} />
+            </div>
+          ))}
+        </div>
+
+        <h2 className="heading my-2">Shayari in roman english romantic</h2>
+        <div className={styles.items}>
+          {romantic.map((card, i) => (
+            <div className={styles.shayari}>
+              <div className={styles.action}>
+                <MdCloudDownload
+                  onClick={(e) =>
+                    toPng(e.currentTarget.parentElement.parentElement)
+                  }
+                  size={25}
+                />
+                <MdContentCopy onClick={(e) => copy(card.shayari)} size={25} />
+              </div>
+              <Image
+                className={styles.img}
+                src={`/shayari/romantic-shayari-4.png`}
+                layout="fill"
+                objectFit="cover"
+                alt="shayari in roman english"
+              />
+
+              <code>{card.shayari}</code>
+            </div>
+          ))}
+        </div>
+
+        <h2 className="heading my-2">Shayari in roman english for friend</h2>
+        <div className={styles.items}>
+          {friend.map((card, i) => (
+            <div className={styles.shayari}>
+              <div className={styles.action}>
+                <MdCloudDownload
+                  onClick={(e) =>
+                    toPng(e.currentTarget.parentElement.parentElement)
+                  }
+                  size={25}
+                />
+                <MdContentCopy onClick={(e) => copy(card.shayari)} size={25} />
+              </div>
+              <Image
+                className={styles.img}
+                src={`/shayari/friend-shayari-2.png`}
+                layout="fill"
+                objectFit="cover"
+                alt="shayari in roman english"
+              />
+
+              <code>{card.shayari}</code>
+            </div>
+          ))}
+        </div>
+        <h2 className="heading my-2">Shayari in roman english love</h2>
+        <div className={styles.items}>
+          {shayaries.map((card, i) => (
+            <div className={styles.shayari}>
+              <div className={styles.action}>
+                <MdCloudDownload
+                  onClick={(e) =>
+                    toPng(e.currentTarget.parentElement.parentElement)
+                  }
+                  size={25}
+                />
+                <MdContentCopy onClick={(e) => copy(card.shayari)} size={25} />
+              </div>
+              <Image
+                className={styles.img}
+                src={`/shayari/romantic-shayari-4.png`}
+                layout="fill"
+                objectFit="cover"
+                alt="shayari in roman english"
+              />
+
+              <code>
+                {card.shayari}
+                <br></br>
+                {card.translation}
+              </code>
+            </div>
+          ))}
+        </div>
+        <h2 className="heading my-2">Shayari in roman english mohabbat</h2>
+        <p className="text-center">10 Mohabbat Shayaris in Roman English</p>
+        <div className={styles.items}>
+          {mohabbat.map((card, i) => (
+            <div className={styles.shayari}>
+              <div className={styles.action}>
+                <MdCloudDownload
+                  onClick={(e) =>
+                    toPng(e.currentTarget.parentElement.parentElement)
+                  }
+                  size={25}
+                />
+                <MdContentCopy onClick={(e) => copy(card.shayari)} size={25} />
+              </div>
+              <Image
+                className={styles.img}
+                src={`/shayari/romantic-shayari-4.png`}
+                layout="fill"
+                objectFit="cover"
+                alt="shayari in roman english"
+              />
+
+              <code>{card.shayari}</code>
+            </div>
+          ))}
+        </div>
+      </article>
+      <Toc />
+      {download && (
+        <DownloadPopup
+          handler={setdownload}
+          src={download.url}
+          styles={styles}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Page;
+// export async function getStaticProps() {
+//   const data = await getShayaries();
+//   return {
+//     props: { data },
+//   };
+// }
---
+++ public/data/shayari.js
@@ -0,0 +1,233 @@
+export const mohabbat = [
+    {
+      shayari: `Mohabbat mein aisa jadoo hai,
+Kho jate hain dil ke tar,
+Ishq mein aisi beqaraari hai,
+Ke jee mein aati hai darr.`,
+    },
+    {
+      shayari: `There is magic in love,
+Heartbeats get lost in it,
+There is such restlessness in love,
+That fear enters the soul.`,
+    },
+    {
+      shayari: `Mohabbat mein koi rang nahin hota,
+Pyaar toh bas dil ka ehsaas hota hai,
+Kuchh log zindagi mein aise aate hain,
+Jaise zindagi khubsurat ho jaati hai.`,
+    },
+    ,
+    {
+      shayari: `There is no color in love,
+Love is just a feeling of the heart,
+Some people come into your life,
+And make your life beautiful.`,
+    },
+    ,
+    {
+      shayari: `Mohabbat ka silsila hai,
+Kuchh khaas log hi jaante hain,
+Dil mein basa hua ehsaas hai,
+Jo khoobsoorat si parchaai hai.`,
+    },
+    ,
+    {
+      shayari: `Love is a connection,
+Only a few special people know,
+It is a feeling that resides in the heart,
+And leaves a beautiful mark.`,
+    },
+    ,
+    {
+      shayari: `Mohabbat mein aisa junoon hai,
+Dil ke tukde hote hain aur phir judaai hai,
+Ishq mein aisi aag hai,
+Jiske samne subah bhi raat hai.`,
+    },
+    ,
+    {
+      shayari: `Mohabbat hai toh saath rehna,
+Saath hai toh khushiyaan bhi hai,
+Khushiyaan hai toh zindagi hai,
+Zindagi hai toh Mohabbat hai.`,
+    },
+  ];
+  export const romantic = [
+    {
+      shayari: `
+    Tumse hi mera safar hai,
+Tumse hi mera pyaar hai,
+Tumse hi meri aashiqui hai.
+    `,
+    },
+    {
+      shayari: `
+      Tum meri zindagi ho,
+Tum meri jaan ho,
+Tum meri aarzoo ho,
+Tum meri khwahish ho.
+      `,
+    },
+    {
+      shayari: `
+      Tujhse hi hai meri duniya,
+Tujhse hi hai mera jahan,
+Tere bina adhuri hai zindagi,
+Tere bina hai zindagi ki har shaam.
+      `,
+    },
+    ,
+    {
+      shayari: `
+      My world is with you,
+My universe is with you,
+Life is incomplete without you,
+Every evening is incomplete without you.`,
+    },
+    ,
+    {
+      shayari: `
+      Teri aankhon mein doob jaane ko dil karta hai,
+Tere saath ek pal bhi bitane ko dil karta hai,
+Tere bina kuchh adhura sa lagta hai,
+Teri yaadon mein kho jaane ko dil karta hai.
+      `,
+    },
+    ,
+    {
+      shayari: `I feel like drowning in your eyes,
+I feel like spending a moment with you,
+Something seems incomplete without you,
+I feel like getting lost in your memories.`,
+    },
+    ,
+    {
+      shayari: `In the light of your love,
+I have boundless love for you,
+I keep getting lost in your memories,
+My life is incomplete without you.`,
+    },
+    ,
+    {
+      shayari: `I love you,
+You are my happiness,
+You are my life,
+You are everything to me.`,
+    },
+  ];
+  export const friend = [
+    {
+      shayari: `
+Dosti ka rishta hai anmol,
+Jo kabhi na tutne wala,
+Tum ho mere saath har pal,
+Aur banaye rakho yeh joda.`,
+    },
+    {
+      shayari: `
+Dost milte hain zindagi mein,
+Par kuchh khaas hote hain,
+Tum jaise dost na mil paaye,
+Agar aaj nahi toh kal toh mil jaaye.`,
+    },
+    {
+      shayari: `
+Dosti ka matlab samjho yaar,
+Ek aag ka dariya hai, aur doob ke jana hai,
+Mujhse bahut dur ho tum,
+Par hamesha mere dil ke karib ho.
+      `,
+    },
+    {
+      shayari: `
+      Dost hote hain dil ke kareeb,
+Jo zindagi ko khushi se bhar de,
+Mere saath aisa hi hai,
+Tumhari dosti mere liye sabse keemti hai.
+      `,
+    },
+    {
+      shayari: `
+Dosti ki hai toh nibhani padegi,
+Har khushi mein tumhare saath muskurana padega,
+Dost bina zindagi adhuri hai,
+Dosti ka saath zindagi bhar nibhana padega.
+      `,
+    },
+    {
+      shayari: `
+Jab bhi hum dost banenge,
+Tabhi yeh zindagi ka maza ayega,
+Khusiyan toh har koi mana leta hai,
+Magar dost banake khush rahenge toh bahut haseen din ayenge.
+      `,
+    },
+    ,
+    {
+      shayari: `
+Zindagi mein dosti ka matlab samjha hai maine,
+Dosti ke saath hansi mazaak karne ka,
+Dosti ke saath gham ko baantne ka,
+Dosti ke saath muskurate hue zindagi jeene ka.
+      `,
+    },
+    {
+      shayari: `
+Har ek dost kamina hota hai,
+Par tu toh sabse bada kamina hai,
+Lekin tere bina dosti adhoori hai,
+Tujhse bada dost koi nahi hai.
+      `,
+    },
+    {
+      shayari: `
+Dost hamesha yaad aate hain,
+Dosti ka rishta toh kabhi khatam nahi hota,
+Dosti ki yeh kahani hai ajeeb,
+Dosti ke siwa kuch nahi hota.
+      `,
+    },
+  ];
+  export const shayaries = [
+    {
+      shayari: "Tumhari aankhon mein ek aasmaan hai",
+      translation: "There is a sky in your eyes",
+    },
+    {
+      shayari: "Dil ko chhoo jane wale shayari",
+      translation: "Heart-touching shayaris",
+    },
+    {
+      shayari: "Kuchh log zindagi mein bahut khaas hote hain",
+      translation: "Some people are very special in life",
+    },
+    {
+      shayari: "Mohabbat hai meri tujhse",
+      translation: "I am in love with you",
+    },
+    {
+      shayari: "Dil ki baatein zubaan pe aane lagi",
+      translation: "The thoughts of the heart have started to come on the lips",
+    },
+    {
+      shayari: "Kuchh khwaab sajaane hain",
+      translation: "I have to fulfill some dreams",
+    },
+    {
+      shayari: "Zindagi ka safar hai yeh",
+      translation: "This is the journey of life",
+    },
+    {
+      shayari: "Har lamha khubsurat hai",
+      translation: "Every moment is beautiful",
+    },
+    {
+      shayari: "Kuchh toh hai tujhse raabta",
+      translation: "There is some connection with you",
+    },
+    {
+      shayari: "Mohabbat ki hai inteha par",
+      translation: "Love has reached its limit",
+    },
+  ];
