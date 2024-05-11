79d2e36aaefccba2b369fafe6efef93d4edb2a93
Ashish Kumar <ashishbhu221306@gmail.com>
2024-05-11 20:36:15 +0530

    fixed

---
+++ pages/test.js
@@ -0,0 +1,11 @@
+import { NextSeo } from "next-seo";
+import React from "react";
+
+const Page = () => {
+    return (
+        <div><NextSeo noindex />Page
+        </div>
+    );
+};
+
+export default Page;
