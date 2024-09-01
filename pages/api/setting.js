0b4feb55fa5f7b6fb8e0defc408e739afaf4b885
Ashish Kumar <ashishbhu221306@gmail.com>
2024-09-01 17:24:05 +0530

    setting api fix

--- pages/api/setting.js
+++ pages/api/setting.js
@@ -1,9 +1,11 @@
 import { writeFile } from "fs/promises";
+import path from "path";
 
 export default async function handler(req, res) {
+    const filepath = path.join(process.cwd(), req.body.path);
     try {
-        const { data, path } = req.body;
-        await writeFile(path,data);
+        const { data } = req.body;
+        await writeFile(filepath, data);
         res.json({
             success: true,
             message: "settings updated",
