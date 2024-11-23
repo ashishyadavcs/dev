5d0e3155564693180f1d5b84f2731b19d6b4919d
Ashish Kumar <ashishbhu221306@gmail.com>
2024-11-23 15:36:38 +0530

    title added of AI page

--- pages/ai.js
+++ pages/ai.js
@@ -1,9 +1,11 @@
 import Geminiai from "@/components/geminiai";
+import {NextSeo} from "next-seo";
 import styled from "styled-components";
 
 const Page = () => {
     return (
         <StyledAI className="container">
+            <NextSeo title="gemini ai | Ask your questions with AI"/>
             <h1 className="text-center">Ask your question with AI</h1>
             <Geminiai />
         </StyledAI>
